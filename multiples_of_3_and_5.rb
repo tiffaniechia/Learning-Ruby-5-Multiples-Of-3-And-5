588c311f774f019d125f2b0bd46a9718665efdfd
tiffanie <yt.chia.2009@smu.edu.sg>
2014-02-26 17:15:55 +0000

    first complete

---
+++ multiples_of_3_and_5.rb
@@ -0,0 +1,29 @@
+def play(num)
+	return num if divisable_by_three?(num) || divisable_by_five?(num)
+ 	return 0
+end
+
+def divisable_by_fifteen?(num)
+	num % 15 == 0
+end
+
+def divisable_by_three?(num)
+	num % 3 == 0 
+end
+
+def divisable_by_five?(num)
+	num % 5 == 0
+end
+ 
+def game(max)
+num = 1
+sum = 0 
+loop do
+	sum += play(num)
+	num += 1
+	break if num >= max
+end
+sum
+end
+
+puts game(1000)
